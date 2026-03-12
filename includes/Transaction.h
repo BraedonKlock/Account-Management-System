d245fce88d0e8f71b73100c0c3fe9b0fb52e5512
Braedon Klock <braedonk@hotmail.com>
2026-03-12 10:55:11 -0400

    IMPLEMENTED: class members and parameterized constructor for Transaction class

--- includes/Transaction.h
+++ includes/Transaction.h
@@ -1,8 +1,20 @@
 #pragma once
 
-class Transaction {
-public:
+#include <string>
+#include <time_t>
 
+class Transaction {
+protected:
+	const std::string transactionId;
+	const std::string initiatorAccountId;
+	const std::time_t timeStamp;
+public:
+	Transaction(const std::string transactionId, const std::string initiatorAccountId, const std::time_t timeStamp);
+	
+	virtual bool validate() = 0;
+	virtual std::string getType() const = 0;
+
+	virtual ~Transaction() = default;
 private:
 
 }
--- src/Transaction.cpp
+++ src/Transaction.cpp
@@ -1,1 +1,10 @@
 #include "Transaction.h"
+
+#include <string>
+#include <time_t>
+
+Transaction::Transaction(const std::string transactionId, const std::string initiatorAccountId, const std::time_t timeStamp) 
+	: transactionId(transactionId),
+	  initiatorAccountId(initiatorAccountId),
+	  timeStamp(timeStamp) {
+}
