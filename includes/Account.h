30edc8d47da238b989b72469214121d5fd5837a8
Braedon Klock <braedonk@hotmail.com>
2026-03-14 11:57:11 -0400

    UPDATED: main.cpp with new menu options.REMOVED: class members

--- includes/Account.h
+++ includes/Account.h
@@ -5,11 +5,8 @@
 class Account
 {
 private:
-	std::string address;
 	double balance;
 public:
-	Address(const std::string& address, double balance = 0.0);
-	const std::string& getAddress() const;
 	double getBalance() const;
 }
 
