2b1bdcebc630e5014121db5f23cdcbc6b92a0ffe
Braedon Klock <braedonk@hotmail.com>
2026-03-28 17:13:53 -0400

    set copy and move constructors and assignment operators to equal delete so that they cant be used.

--- includes/accounts/account.h
+++ includes/accounts/account.h
@@ -6,7 +6,13 @@ class Account
 {
 protected:
 	double balance = 0.0;
+	Account() = default;
 public:
+	Account(const Account&) = delete;
+	Account& operator=(const Account&) = delete;
+	Account(Account&&) = delete;
+	Account& operator=(Account&&) = delete;
+
 	double getBalance() const;
 	virtual bool withdraw(double amount) = 0;
 	virtual bool deposit(double amount) = 0;
